23e30b339bbaf4d685b6ea0065d89c7bef0ee1f2
MaoShu SRE <maoshu@maoshu-MacBook.Air>
2024-09-13 15:33:55 +0800

    change node 1 to cluster 3

--- argocd/argocd-clickhouse-posthog/create-stack.sh
+++ argocd/argocd-clickhouse-posthog/create-stack.sh
@@ -1,32 +1,37 @@
 #!/bin/bash
-NS=${NAMESPACE:-ch}
+NS=${NAMESPACE:-clickhouse}
 echo "This command will install the analytic stack in namespace ${NS}"
 echo -n "Press enter or ^c to cancel: "
 read
 set -x
 argocd app create clickhouse-operator \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/clickhouse-operator \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appsclickhouse-operator \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+
 argocd app create prometheus \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/prometheus \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appsprometheus \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+
 argocd app create grafana \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/grafana \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appsgrafana \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+
 argocd app create zookeeper \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/zookeeper \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appszookeeper \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+
 argocd app create clickhouse \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/clickhouse \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appsclickhouse \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+
 argocd app create cloudbeaver \
- --repo https://github.com/Altinity/argocd-examples-clickhouse.git \
- --path apps/cloudbeaver \
+ --repo https://github.com/ranmaoshu1/datastar.git \
+ --path argocd/argocd-clickhouse-posthog/appscloudbeaver \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 argocd app sync clickhouse-operator
 argocd app sync prometheus
