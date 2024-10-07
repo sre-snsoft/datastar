890199b1032cdd1ecdbd8180e6a533006b3b63ad
MaoShu SRE <maoshu@maoshu-MacBook.Air>
2024-10-07 14:32:40 +0800

    add uat cpms k8s-otel-infra

--- flink/argocd-dos-flink/create-stack.sh
+++ flink/argocd-dos-flink/create-stack.sh
@@ -1,43 +1,25 @@
 #!/bin/bash
-NS=${NAMESPACE:-ch228}
+NS=${NAMESPACE:-monitor}
 echo "This command will install the analytic stack in namespace ${NS}"
 echo -n "Press enter or ^c to cancel: "
 read
 set -x
-argocd app create clickhouse-operator \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/clickhouse-operator \
+argocd app create prometheus \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path flink/argocd-dos-flink/apps/prometheus \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
-argocd app create prometheus \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/prometheus \
- --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
+kubectl create secret generic grafana --from-literal=username=admin --from-literal=admin-password= -n monitor
+kubectl create secret generic grafana --from-literal=admin-username=admin --dry-run=client -o yaml | kubectl apply -f - -n monitor
 
 argocd app create grafana \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/grafana \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path flink/argocd-dos-flink/apps/grafana \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
-argocd app create zookeeper \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/zookeeper \
- --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
-
-argocd app create clickhouse \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/clickhouse \
- --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
-
-argocd app create cloudbeaver \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/cloudbeaver \
- --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
-
-argocd app sync clickhouse-operator
 argocd app sync prometheus
 argocd app sync grafana
-argocd app sync zookeeper
-argocd app sync clickhouse
-argocd app sync cloudbeaver
+
+#
+# echo "nvidia_smi_cmd_pod_status 0" | curl --data-binary @- http://a41093c9812b64ed7957852531a4ad74-1203801685.ap-southeast-1.elb.amazonaws.com:9091/metrics/job/prometheus-pushgateway/cluster/ops-k8s-cluster/instance/ops-k8s-2/pod_name/gpu-server-0
 
--- flink/flink-kubernetes-operator/flink-dos-realtime-coupon/build.sh
+++ flink/flink-kubernetes-operator/flink-dos-realtime-coupon/build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #docker 更新
-docker build -t flink:dos_realtime_coupon .
+docker build -t flink:dos_realtime_coupon_v3 .
 
-docker tag flink:dos_realtime_coupon 545009824097.dkr.ecr.ap-southeast-1.amazonaws.com/flink:dos_realtime_coupon_v1
+docker tag flink:dos_realtime_coupon_v3 545009824097.dkr.ecr.ap-southeast-1.amazonaws.com/flink:dos_realtime_coupon_v3
 
-docker push 545009824097.dkr.ecr.ap-southeast-1.amazonaws.com/flink:dos_realtime_coupon_v1
+docker push 545009824097.dkr.ecr.ap-southeast-1.amazonaws.com/flink:dos_realtime_coupon_v3
 
 # eks 启动
 kubectl delete -f flink-dos-realtime-coupon.yaml
