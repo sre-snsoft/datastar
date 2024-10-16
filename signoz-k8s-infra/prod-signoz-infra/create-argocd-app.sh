0a159fc2de7da0cd3c14dc7ccd6bd4fe44fb6f3f
MaoShu SRE <maoshu@maoshu-MacBook.Air>
2024-10-16 17:07:49 +0800

    add signoz prod k8s infra argocd

---
+++ signoz-k8s-infra/prod-signoz-infra/create-argocd-app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+NS=${NAMESPACE:-signoz-otel}
+echo "This command will install the analytic stack in namespace ${NS}"
+echo -n "Press enter or ^c to cancel: "
+read
+set -x
+argocd app create signoz-k8s-infra \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path signoz-k8s-infra/uat-fpms-signoz-infra \
+ --file override-values.yaml \
+ --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
