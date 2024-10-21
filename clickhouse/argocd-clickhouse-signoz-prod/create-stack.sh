1e5eee735305078d40c292d5c3ae8784b747bf6f
MaoShu SRE <maoshu@maoshu-MacBook.Air>
2024-10-21 16:18:42 +0800

    add

--- clickhouse/argocd-clickhouse-signoz-prod/create-stack.sh
+++ clickhouse/argocd-clickhouse-signoz-prod/create-stack.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
-NS=${NAMESPACE:-ch228}
+NS=${NAMESPACE:-clickhouse}
 echo "This command will install the analytic stack in namespace ${NS}"
 echo -n "Press enter or ^c to cancel: "
 read
 set -x
 argocd app create clickhouse-operator \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/clickhouse-operator \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/clickhouse-operator \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app create prometheus \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/prometheus \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/prometheus \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app create grafana \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/grafana \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/grafana \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app create zookeeper \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/zookeeper \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/zookeeper \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app create clickhouse \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/clickhouse \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/clickhouse \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app create cloudbeaver \
- --repo https://github.com/ranmaoshu1/datastar.git \
- --path clickhouse/argocd-clickhouse-posthog/apps/cloudbeaver \
+ --repo https://github.com/sre-snsoft/datastar.git \
+ --path clickhouse/argocd-clickhouse-signoz-prod/apps/cloudbeaver \
  --dest-server https://kubernetes.default.svc --dest-namespace ${NS}
 
 argocd app sync clickhouse-operator
